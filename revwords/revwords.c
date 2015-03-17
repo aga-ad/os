3df6829d0d9ece49defac41ec5d2ee801d9a571f
Aganov <aganov.a.d@gmail.com>
2015-03-18 02:36:00 +0300

    fixed 4096b words and deleted xor from reverse function

--- revwords/revwords.c
+++ revwords/revwords.c
@@ -3,16 +3,18 @@
 
 void reverse(char* buf, size_t size) {
 	size_t i;
+	char t;
 	for (i = 0; i < size / 2; i++) {
-		buf[i] ^= buf[size - 1 - i];
-		buf[size - 1 - i] ^= buf[i];
-		buf[i] ^= buf[size - 1 - i];
+		t = buf[size - 1 - i];
+		buf[size - 1 - i] = buf[i];
+		buf[i] = t;
 	}
 }
 
 int main() {
     const size_t BUFFER_CAPACITY = 4096;
-	char buf[BUFFER_CAPACITY];
+    const size_t MAX_WORD_LENGTH = 4096;//"Гарантируется, что каждое слово имеет длину не более 4096 байт."
+	char buf[BUFFER_CAPACITY + 1];
 	int i, l;
 	ssize_t read_size, write_size, buffer_size;
 	buffer_size = 0;
@@ -44,6 +46,14 @@ int main() {
 				}
 				l = i + 1;
 			}
+		}
+		if (buffer_size == MAX_WORD_LENGTH) {
+			reverse(buf, buffer_size);
+			write_size = write_(STDOUT_FILENO, buf, buffer_size);
+			if (write_size == -1) {
+				return 1;
+			}
+			l = buffer_size;
 		}
 		buffer_size -= l;
 		for (i = 0; i < buffer_size; i++) {
