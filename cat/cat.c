719ef1c2a2fe114f2130019f8eb5bc687fd1587d
Aganov <aganov.a.d@gmail.com>
2015-03-25 01:33:48 +0300

    tabs fixed

--- cat/cat.c
+++ cat/cat.c
@@ -3,18 +3,18 @@
 
 int main() {
     const size_t BUFFER_SIZE = 4096;
-	char buf[BUFFER_SIZE];
-	while (1) {
-		ssize_t read_size = read_(STDIN_FILENO, buf, BUFFER_SIZE);
-		if (read_size == -1) {
-			return 1;
-		}
-		if (read_size == 0) {
-			return 0;
-		}
-		ssize_t write_size = write_(STDOUT_FILENO, buf, read_size);
-		if (write_size == -1) {
-			return 1;
-		}
-	}
+    char buf[BUFFER_SIZE];
+    while (1) {
+        ssize_t read_size = read_(STDIN_FILENO, buf, BUFFER_SIZE);
+        if (read_size == -1) {
+            return 1;
+        }
+        if (read_size == 0) {
+            return 0;
+        }
+        ssize_t write_size = write_(STDOUT_FILENO, buf, read_size);
+        if (write_size == -1) {
+            return 1;
+        }
+    }
 }
--- revwords/revwords.c
+++ revwords/revwords.c
@@ -2,62 +2,62 @@
 #include <stdio.h>
 
 void reverse(char* buf, size_t size) {
-	size_t i;
-	char t;
-	for (i = 0; i < size / 2; i++) {
-		t = buf[size - 1 - i];
-		buf[size - 1 - i] = buf[i];
-		buf[i] = t;
-	}
+    size_t i;
+    char t;
+    for (i = 0; i < size / 2; i++) {
+        t = buf[size - 1 - i];
+        buf[size - 1 - i] = buf[i];
+        buf[i] = t;
+    }
 }
 
 int main() {
     const size_t BUFFER_CAPACITY = 4096;
     const size_t MAX_WORD_LENGTH = 4096;//"Гарантируется, что каждое слово имеет длину не более 4096 байт."
-	char buf[BUFFER_CAPACITY];
-	int i, l;
-	ssize_t read_size, write_size, buffer_size;
-	buffer_size = 0;
-	while (1) {
-		read_size = read_until(STDIN_FILENO, buf + buffer_size, BUFFER_CAPACITY - buffer_size, ' ');
-		if (read_size == -1) {
-			return 1;
-		}
-		if (read_size == 0) {
-			if (buffer_size == 0) {
-				return 0;
-			}
-			reverse(buf, buffer_size);
-			write_size = write_(STDOUT_FILENO, buf, buffer_size);
-			if (write_size == -1) {
-				return 1;
-			}
-			return 0;
-		}
-		i = buffer_size;
-		buffer_size += read_size;
-		l = 0;
-		for (; i < buffer_size; i++) {
-			if (buf[i] == ' ') {
-				reverse(buf + l, i - l);
-				write_size = write_(STDOUT_FILENO, buf + l, i - l + 1);
-				if (write_size == -1) {
-					return 1;
-				}
-				l = i + 1;
-			}
-		}
-		if (buffer_size - l == MAX_WORD_LENGTH) {
-			reverse(buf, buffer_size);
-			write_size = write_(STDOUT_FILENO, buf, buffer_size);
-			if (write_size == -1) {
-				return 1;
-			}
-			l = buffer_size;
-		}
-		buffer_size -= l;
-		for (i = 0; i < buffer_size; i++) {
-			buf[i] = buf[i + l];
-		}
-	}
+    char buf[BUFFER_CAPACITY];
+    int i, l;
+    ssize_t read_size, write_size, buffer_size;
+    buffer_size = 0;
+    while (1) {
+        read_size = read_until(STDIN_FILENO, buf + buffer_size, BUFFER_CAPACITY - buffer_size, ' ');
+        if (read_size == -1) {
+            return 1;
+        }
+        if (read_size == 0) {
+            if (buffer_size == 0) {
+                return 0;
+            }
+            reverse(buf, buffer_size);
+            write_size = write_(STDOUT_FILENO, buf, buffer_size);
+            if (write_size == -1) {
+                return 1;
+            }
+            return 0;
+        }
+        i = buffer_size;
+        buffer_size += read_size;
+        l = 0;
+        for (; i < buffer_size; i++) {
+            if (buf[i] == ' ') {
+                reverse(buf + l, i - l);
+                write_size = write_(STDOUT_FILENO, buf + l, i - l + 1);
+                if (write_size == -1) {
+                    return 1;
+                }
+                l = i + 1;
+            }
+        }
+        if (buffer_size - l == MAX_WORD_LENGTH) {
+            reverse(buf, buffer_size);
+            write_size = write_(STDOUT_FILENO, buf, buffer_size);
+            if (write_size == -1) {
+                return 1;
+            }
+            l = buffer_size;
+        }
+        buffer_size -= l;
+        for (i = 0; i < buffer_size; i++) {
+            buf[i] = buf[i + l];
+        }
+    }
 }
